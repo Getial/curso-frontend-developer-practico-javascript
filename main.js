d0ce153c7477c94ea7bb131af898c982c3438fa6
Felipe Getial <brayan.getial@peaku.io>
2023-06-12 22:30:45 -0500

    empezando a usar Platzi Fake API

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const shoppingCarIcon = document.querySelector('.navbar-shopping-cart')
+const arrowBackIcon = document.querySelector('.arrow-left-shopping-cart')
 const productDetailClose = document.querySelector('.product-detail-close')
 const burgerMenu = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartContainer = document.querySelector('.order-detail');
 const productDetailContainer = document.querySelector('#productDetail');
 const cardsContainer = document.querySelector('.cards-container')
+const categoriesContainer = document.querySelector('.navbar-categories')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 burgerMenu.addEventListener('click', toggleMobileMenu)
 shoppingCarIcon.addEventListener('click', toggleshoppingCartContainer)
 productDetailClose.addEventListener('click', closeProductDetail)
+arrowBackIcon.addEventListener('click', toggleshoppingCartContainer)
+
+const API_PRODUCTS = 'https://api.escuelajs.co/api/v1/products?offset=0&limit=10';
+const API_CATEGORIES = 'https://api.escuelajs.co/api/v1/categories'
+
+let productList = [];
+let categories = [];
+let loading = true;
+let offset = 0;
 
 function toggleDesktopMenu() {
     const isShoppingCartContainerClose = shoppingCartContainer.classList.contains('inactive')
@@ -68,38 +79,14 @@ function closeProductDetail() {
     productDetailContainer.classList.add('inactive')
 }
 
-const productList = [];
-productList.push({
-  name: 'Bike',
-  price: 120,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-});
-productList.push({
-  name: 'Pantalla',
-  price: 220,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-});
-productList.push({
-  name: 'Compu',
-  price: 620,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-});
-productList.push({
-  name: 'Headsets',
-  price: 120,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-});
-
-
-
-function renderProducts(arr) {
+const  renderProducts = (arr) => {
     for (product of arr) {
       const productCard = document.createElement('div');
       productCard.classList.add('product-card');
     
       // product= {name, price, image} -> product.image
       const productImg = document.createElement('img');
-      productImg.setAttribute('src', product.image);
+      productImg.setAttribute('src', product.images[0]);
       productImg.addEventListener('click', openProductDetail);
     
       const productInfo = document.createElement('div');
@@ -110,7 +97,7 @@ function renderProducts(arr) {
       const productPrice = document.createElement('p');
       productPrice.innerText = '$' + product.price;
       const productName = document.createElement('p');
-      productName.innerText = product.name;
+      productName.innerText = product.title;
     
       productInfoDiv.appendChild(productPrice);
       productInfoDiv.appendChild(productName);
@@ -131,4 +118,58 @@ function renderProducts(arr) {
     }
   }
 
-renderProducts(productList)
+const renderCategories = (arr) => {
+    for (category of arr) {
+        const item = document.createElement('li');
+
+        const ancla = document.createElement('a');
+        ancla.setAttribute('href', '/')
+        ancla.innerText = category.name
+
+        item.appendChild(ancla)
+
+        categoriesContainer.appendChild(item)
+    }
+}
+
+const getProducts = (url) => {
+    loading = true
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            productList = [
+                ...productList,
+                ...data
+            ]
+            loading = false;
+            renderProducts(productList)
+        })
+}
+
+const getCategories = (url) => {
+    loading = true
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // categories = data
+            renderCategories(data)
+            loading = false;
+        })
+}
+
+getProducts(API_PRODUCTS) //llamado al API cuando el usuario ingresa a la pagina
+getCategories(API_CATEGORIES)
+
+const onScroll = () => {
+    if (document.body.scrollHeight - window.innerHeight <= window.scrollY) {
+        while(!loading) {
+            offset += 10;
+            const API = `https://api.escuelajs.co/api/v1/products?offset=${offset}&limit=10`
+            getProducts(API)
+            console.log('estoy en el final del scroll')
+        }
+    }
+}
+    
+// llamamos a onScroll cuando el usuario hace scroll
+window.addEventListener('scroll', onScroll) 
